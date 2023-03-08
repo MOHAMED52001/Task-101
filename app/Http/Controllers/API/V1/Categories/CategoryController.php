ec7b7556ffc62d3ca1f46bedd98826e69ad19ab6
MOHAMED52001 <mohamedatef54200@gmail.com>
2023-03-08 16:35:06 +0200

    add category routes and controllers

--- app/Http/Controllers/API/V1/Categories/CategoryController.php
+++ app/Http/Controllers/API/V1/Categories/CategoryController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers\API\V1\Categories;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\V1\Traits\APIResponse;
 
 class CategoryController extends Controller
 {
+    use APIResponse;
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return $categories->count() > 0 ?
+            $this->apiResponse(200, "success", null, $categories) :
+            $this->apiResponse(200, "success", "There Is No Records In Database", "There Is No Records In Database");
     }
 
     /**
@@ -20,30 +28,45 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'max:255', 'string'],
+        ]);
+
+        $department = Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name . '-' . time()),
+        ]);
+        return  $this->apiResponse(201, "success", null, $department);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, Category $category)
     {
-        //
+        return  $this->apiResponse(200, "success", null, $category);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        $request->validate([
+            'name' => [
+                'string',
+                'max:255',
+            ]
+        ]);
+
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name . '-' . time()),
+        ]);
+
+        return  $this->apiResponse(202, "success", null, $category);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Request $request, Category $category)
     {
-        //
+        $category->delete();
+        return  $this->apiResponse(202, "Deleted Successfully");
     }
 }
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\Categories\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\Posts\PostController;
@@ -12,3 +13,7 @@ use App\Http\Controllers\API\V1\Posts\PostController;
 // Posts Routes
 Route::resource('posts', PostController::class);
 // End Posts Routes
+
+// Categories Routes
+Route::resource('categories', CategoryController::class);
+// End Categories Routes
